9c0777242a63d7eadf9d3d0700a64f0ed421e5cb
salargholami <salargholami021@gmail.com>
2024-01-15 03:24:55 +0330

    create nested rote on hotel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import LocationList from "./components/LocationList/LocationList";
 import { Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}>
+          <Route index element={<div>Hotels</div>} />
+          <Route path=":id" element={<div>single hotel</div>} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/components/AppLayout/AppLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AppLayout() {
+  return (
+    <div className="appLayout">
+      <div className="sidebar">sidebar</div>
+      <div className="mapContainer">map</div>
+    </div>
+  );
+}
+
+export default AppLayout;
